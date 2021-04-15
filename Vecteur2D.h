afac80f82501b56e8a78faf047306ed89fc6a207
yasrey10 <yasrey1710@gmail.com>
2021-04-15 15:14:04 +0200

    hh

---
+++ Vecteur2D.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <cmath>
+class Vecteur2D{
+	private:
+	//attributs
+	double x_;
+	double y_;
+	
+	public:
+	//constructeur
+	Vecteur2D(double x=0, double y=0):x_(x),y_(y){};
+	double get_x();
+	double get_y();
+	void set_coord(double x,double y); 
+
+	//method de class
+	std::ostream& affiche(std::ostream& out) const;
+	//bool compare(const Vecteur2D& autre) const; plus besoin à priori
+	/*Vecteur2D addition(const Vecteur2D& autre) const;*/
+	Vecteur2D soustraction(const Vecteur2D& autre) const;
+	Vecteur2D oppose() const;
+	Vecteur2D mult(double k) const;
+	double prod_scal(const Vecteur2D& autre) const;
+	double norme() const;
+	double norme2() const;
+	/* Vecteur2D unitaire() const; */
+	//operateur
+	bool operator==(Vecteur2D const& autre) const;
+	bool operator!=(Vecteur2D const& autre) const;
+	void operator+=(Vecteur2D const& autre);
+	void operator-=(Vecteur2D const& autre);
+	/* ~ est operateur norme*/
+	Vecteur2D operator~() const;
+
+};
+
+const Vecteur2D operator+(Vecteur2D v1,Vecteur2D const& v2);
+const Vecteur2D operator-(Vecteur2D v1,Vecteur2D const& v2);
+std::ostream& operator<<(std::ostream& out, Vecteur2D const& v);
